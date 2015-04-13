5e91b4dee324d5aea3cc62cb9854be2352bcd2d0
anarkia1976 <stefano.villa1976@gmail.com>
2015-04-13 06:29:13 +0200

    cpufreq: zzmoove: add new @zanezam governor
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    
    Conflicts:
    	drivers/cpufreq/Kconfig

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -398,6 +398,9 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
 #endif
 
 
