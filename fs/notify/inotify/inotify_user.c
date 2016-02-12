fa897a6f179b8aeff322c5b7d1db12b52bdf8ebd
Daniel Rosenberg <drosen@google.com>
2016-02-11 16:44:15 -0800

    vfs: add d_canonical_path for stacked filesystem support
    Inotify does not currently know when a filesystem
    is acting as a wrapper around another fs. This means
    that inotify watchers will miss any modifications to
    the base file, as well as any made in a separate
    stacked fs that points to the same file.
    d_canonical_path solves this problem by allowing the fs
    to map a dentry to a path in the lower fs. Inotify
    can use it to find the appropriate place to watch to
    be informed of all changes to a file.
    
    Change-Id: I09563baffad1711a045e45c1bd0bd8713c2cc0b6
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- fs/notify/inotify/inotify_user.c
+++ fs/notify/inotify/inotify_user.c
@@ -751,6 +751,8 @@ SYSCALL_DEFINE3(inotify_add_watch, int, fd, const char __user *, pathname,
 	struct fsnotify_group *group;
 	struct inode *inode;
 	struct path path;
+	struct path alteredpath;
+	struct path *canonical_path = &path;
 	struct file *filp;
 	struct path alteredpath;
 	struct path *canonical_path = &path;
@@ -862,7 +864,7 @@ static int __init inotify_user_setup(void)
 	BUILD_BUG_ON(IN_ISDIR != FS_ISDIR);
 	BUILD_BUG_ON(IN_ONESHOT != FS_IN_ONESHOT);
 
-	BUG_ON(hweight32(ALL_INOTIFY_BITS) != 21);
+	BUG_ON(hweight32(ALL_INOTIFY_BITS) != 22);
 
 	inotify_inode_mark_cachep = KMEM_CACHE(inotify_inode_mark, SLAB_PANIC);
 	event_priv_cachep = KMEM_CACHE(inotify_event_private_data, SLAB_PANIC);
