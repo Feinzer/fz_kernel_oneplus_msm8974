cac7830b44d9b55dd469d998c533c57a05017aa0
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-11-21 14:01:10 +0530

    bam_dmux: Give just enough time to complete

--- arch/arm/mach-msm/bam_dmux.c
+++ arch/arm/mach-msm/bam_dmux.c
@@ -230,8 +230,8 @@ static struct srcu_struct bam_dmux_srcu;
 /* A2 power collaspe */
 #define UL_TIMEOUT_DELAY 1000	/* in ms */
 #define ENABLE_DISCONNECT_ACK	0x1
-#define SHUTDOWN_TIMEOUT_MS	500
-#define UL_WAKEUP_TIMEOUT_MS	2000
+#define SHUTDOWN_TIMEOUT_MS	250
+#define UL_WAKEUP_TIMEOUT_MS	4000
 static void toggle_apps_ack(void);
 static void reconnect_to_bam(void);
 static void disconnect_to_bam(void);
