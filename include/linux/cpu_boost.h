76aa1925387d397070d9694af9df776c04101cf1
Francisco Franco <franciscofranco.1990@gmail.com>
2018-11-02 03:44:03 +0000

    cpu-boost: add a function to boost the cpu to max freq
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

---
+++ include/linux/cpu_boost.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2018, Francisco Franco <franciscofranco.1990@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef _CPU_BOOST_H_
+#define _CPU_BOOST_H_
+
+void do_input_boost_max(void);
+
+#endif /* _CPUBOOST_H_ */
