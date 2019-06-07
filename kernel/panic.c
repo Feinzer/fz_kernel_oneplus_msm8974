3909d4554091f7a55324e86a59e206f76c552717
Feinzer <danhernan23i@gmail.com>
2019-06-07 03:45:13 +0000

    b r u h

--- kernel/panic.c
+++ kernel/panic.c
@@ -116,7 +116,7 @@ void panic(const char *fmt, ...)
 	va_start(args, fmt);
 	vsnprintf(buf, sizeof(buf), fmt, args);
 	va_end(args);
-	printk(KERN_EMERG "Kernel panic - not syncing: %s\n",buf);
+	printk(KERN_EMERG "Bruh moment - not syncing: %s\n",buf);
 #ifdef CONFIG_DEBUG_BUGVERBOSE
 	/*
 	 * Avoid nested stack-dumping if a panic occurs during oops processing
