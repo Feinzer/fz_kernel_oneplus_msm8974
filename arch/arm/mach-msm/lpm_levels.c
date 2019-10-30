AOSPA/android_kernel_lge_hammerhead

ARM: mach-msm: Fix numerous section mismatches
* Qualcomm writes great code /s

Signed-off-by: Evan Anderson <evananderson@aospa.co>

arch/arm/mach-msm/lpm_levels.c
@@ -1123,7 +1123,7 @@ static struct of_device_id cpu_modes_mtch_tbl[] __initdata = {
 	{},
 };
 
-static struct platform_driver cpu_modes_driver = {
+static struct platform_driver cpu_modes_driver __initdata = {
 	.probe = lpm_cpu_probe,
 	.driver = {
 		.name = "cpu-modes",
@@ -1137,7 +1137,7 @@ static struct of_device_id system_modes_mtch_tbl[] __initdata = {
 	{},
 };
 
-static struct platform_driver system_modes_driver = {
+static struct platform_driver system_modes_driver __initdata = {
 	.probe = lpm_system_probe,
 	.driver = {
 		.name = "system-modes",
@@ -1151,7 +1151,7 @@ static struct of_device_id lpm_levels_match_table[] __initdata = {
 	{},
 };
 
-static struct platform_driver lpm_levels_driver = {
+static struct platform_driver lpm_levels_driver __initdata = {
 	.probe = lpm_probe,
 	.driver = {
 		.name = "lpm-levels",

arch/arm/mach-msm/msm-pm.c
@@ -997,7 +997,7 @@ static struct of_device_id msm_slp_sts_match_tbl[] __initdata= {
 	{},
 };
 
-static struct platform_driver msm_cpu_status_driver = {
+static struct platform_driver msm_cpu_status_driver __initdata = {
 	.probe = msm_cpu_status_probe,
 	.driver = {
 		.name = "cpu_slp_status",
@@ -1011,7 +1011,7 @@ static struct of_device_id msm_snoc_clnt_match_tbl[] __initdata = {
 	{},
 };
 
-static struct platform_driver msm_cpu_pm_snoc_client_driver = {
+static struct platform_driver msm_cpu_pm_snoc_client_driver __initdata = {
 	.probe = msm_pm_snoc_client_probe,
 	.driver = {
 		.name = "pm_snoc_client",
@@ -1278,7 +1278,7 @@ static struct of_device_id msm_cpu_pm_table[] __initdata = {
 	{},
 };
 
-static struct platform_driver msm_cpu_pm_driver = {
+static struct platform_driver msm_cpu_pm_driver __initdata = {
 	.probe = msm_cpu_pm_probe,
 	.driver = {
 		.name = "pm-8x60",

arch/arm/mach-msm/rpm-smd.c
@@ -1502,7 +1502,7 @@ static struct of_device_id msm_rpm_match_table[] __initdata =  {
 	{},
 };
 
-static struct platform_driver msm_rpm_device_driver = {
+static struct platform_driver msm_rpm_device_driver __initdata = {
 	.probe = msm_rpm_dev_probe,
 	.driver = {
 		.name = "rpm-smd",

Feinzer/fz_kernel_oneplus_msm8974

Change-Id: Ibfc1cf5196c9ee33e2f06b689d22976eb1109868
Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

arch/arm/mach-msm/lpm_levels.c
@@ -1122,7 +1122,7 @@ static struct of_device_id cpu_modes_mtch_tbl[] __initdata = {
 	{},
 };
 
-static struct platform_driver cpu_modes_driver = {
+static struct platform_driver cpu_modes_driver __refdata = {
 	.probe = lpm_cpu_probe,
 	.driver = {
 		.name = "cpu-modes",
@@ -1136,7 +1136,7 @@ static struct of_device_id system_modes_mtch_tbl[] __initdata = {
 	{},
 };
 
-static struct platform_driver system_modes_driver = {
+static struct platform_driver system_modes_driver __refdata = {
 	.probe = lpm_system_probe,
 	.driver = {
 		.name = "system-modes",
@@ -1150,7 +1150,7 @@ static struct of_device_id lpm_levels_match_table[] __initdata = {
 	{},
 };
 
-static struct platform_driver lpm_levels_driver = {
+static struct platform_driver lpm_levels_driver __refdata = {
 	.probe = lpm_probe,
 	.driver = {
 		.name = "lpm-levels",

arch/arm/mach-msm/msm-pm.c
@@ -996,7 +996,7 @@ static struct of_device_id msm_slp_sts_match_tbl[] __initdata= {
 	{},
 };
 
-static struct platform_driver msm_cpu_status_driver = {
+static struct platform_driver msm_cpu_status_driver __refdata = {
 	.probe = msm_cpu_status_probe,
 	.driver = {
 		.name = "cpu_slp_status",
@@ -1010,7 +1010,7 @@ static struct of_device_id msm_snoc_clnt_match_tbl[] __initdata = {
 	{},
 };
 
-static struct platform_driver msm_cpu_pm_snoc_client_driver = {
+static struct platform_driver msm_cpu_pm_snoc_client_driver __refdata = {
 	.probe = msm_pm_snoc_client_probe,
 	.driver = {
 		.name = "pm_snoc_client",
@@ -1277,7 +1277,7 @@ static struct of_device_id msm_cpu_pm_table[] __initdata = {
 	{},
 };
 
-static struct platform_driver msm_cpu_pm_driver = {
+static struct platform_driver msm_cpu_pm_driver __refdata = {
 	.probe = msm_cpu_pm_probe,
 	.driver = {
 		.name = "pm-8x60",

arch/arm/mach-msm/rpm-smd.c
@@ -1459,7 +1459,7 @@ static struct of_device_id msm_rpm_match_table[] __initdata =  {
 	{},
 };
 
-static struct platform_driver msm_rpm_device_driver = {
+static struct platform_driver msm_rpm_device_driver __refdata = {
 	.probe = msm_rpm_dev_probe,
 	.driver = {
 		.name = "rpm-smd",